c4691f0e55dded696bc2400e140cc90cf268648b
Cuong Ho <cuong.ict.10101@gmail.com>
2018-02-23 07:20:34 +0700

    Add styled-components

---
+++ src/views/pages/weather/SearchBar.test.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import { mount } from 'enzyme';
+import SearchBar from './SearchBar';
+
+describe('SearchBar', () => {
+  it('triggers successful fetch weather info action when enters correct location', () => {
+    const fetchWeatherInfoMock = jest.func  
+  });
+});
--- src/views/pages/weather/index.js
+++ src/views/pages/weather/index.js
@@ -1,13 +1,18 @@
 import React from 'react';
-
+import styled from 'styled-components';
 import SearchBarContainer from './../../containers/search-bar';
 import WeatherInfoContainer from './../../containers/weather-info';
 
+const MainLayout = styled.div`
+  background: pink;
+  text-align: center;
+`;
+
 const Weather = () => (
-  <div className='weather'>
+  <MainLayout>
     <SearchBarContainer />
     <WeatherInfoContainer />
-  </div>
+  </MainLayout>
 );
 
 export default Weather;
